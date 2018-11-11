3d0828cf3da13722f1e9915fcbbba80591d39a2e
HanInfinity <haninfinity530@gmail.com>
2018-11-12 01:42:33 +0800

    更新day27素材

---
+++ article/day27_foliumKaoDist/folium_KaoVill.py
@@ -0,0 +1,8 @@
+import folium
+
+myMap = folium.Map([22.73444963475145, 120.28458595275877], zoom_start=14)
+kaoDistrict = folium.GeoJson('../../dist/mapdata/KaoVillageRange.json')
+
+kaoDistrict.add_to(myMap)
+myMap.fit_bounds(kaoDistrict.get_bounds())
+myMap.save('Folium_KaoVill.html')
