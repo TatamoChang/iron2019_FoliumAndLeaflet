3b3d90f1ecc06dbc349d02da232e33a712e64fc8
HanInfinity <haninfinity530@gmail.com>
2018-11-13 01:41:13 +0800

    day28資料更新

--- article/day28_showInformation/folium_KaoVill.py
+++ article/day28_showInformation/folium_KaoVill.py
@@ -1,9 +1,26 @@
 import folium
+import pandas as pd
+
+newList = []
+def switchLatLng(geos):
+    for geo in geos:
+        if isinstance(geo[0],list):
+            switchLatLng(geo)
+        else:
+            newList.append([geo[1], geo[0]])
+    return newList
 
 myMap = folium.Map([22.73444963475145, 120.28458595275877], zoom_start=14)
-
-kaoDistrict = folium.GeoJson('../../dist/mapdata/KaoVillageRange.json')
-
-kaoDistrict.add_to(myMap)
-myMap.fit_bounds(kaoDistrict.get_bounds())
-myMap.save('Folium_KaoVill.html')
+kaoVill = folium.FeatureGroup()
+i = 1
+for feature in pd.read_json('../../dist/mapdata/KaoVillageRange.json')["features"]:
+    villpopup = '這裡是<h3>'+feature['properties']['TV_ALL']+'</h3>總面積為'+feature['properties']['AREA']+'平方公尺'
+    coors = [switchLatLng(feature['geometry']['coordinates'])]
+    villRange = folium.Polygon(coors, 
+                               popup = villpopup,
+                               fill = True).add_to(kaoVill)
+    i = i + 1
+    print(i)
+kaoVill.add_to(myMap)
+myMap.fit_bounds(kaoVill.get_bounds())
+myMap.save('Folium_KaoVillpopup.html')
