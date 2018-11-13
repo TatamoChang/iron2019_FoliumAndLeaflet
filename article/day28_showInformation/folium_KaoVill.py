1c5cf145a3bc35cc1ceaff9a625dc1e2a26d5e03
HanInfinity <haninfinity530@gmail.com>
2018-11-13 23:44:27 +0800

    完成day29
    終於成功了！！！

--- article/day28_showInformation/folium_KaoVill.py
+++ article/day28_showInformation/folium_KaoVill.py
@@ -10,17 +10,16 @@ def switchLatLng(geos):
             newList.append([geo[1], geo[0]])
     return newList
 
-myMap = folium.Map([22.73444963475145, 120.28458595275877], zoom_start=14)
+myMap = folium.Map([22.73444963475145, 120.28458595275877], zoom_start=6)
 kaoVill = folium.FeatureGroup()
-i = 1
-for feature in pd.read_json('../../dist/mapdata/KaoVillageRange.json')["features"]:
+features = pd.read_json('../../dist/mapdata/KaoVillageRange.json')["features"]
+for feature in features:
     villpopup = '這裡是<h3>'+feature['properties']['TV_ALL']+'</h3>總面積為'+feature['properties']['AREA']+'平方公尺'
     coors = [switchLatLng(feature['geometry']['coordinates'])]
+    newList = []
     villRange = folium.Polygon(coors, 
                                popup = villpopup,
                                fill = True).add_to(kaoVill)
-    i = i + 1
-    print(i)
 kaoVill.add_to(myMap)
 myMap.fit_bounds(kaoVill.get_bounds())
 myMap.save('Folium_KaoVillpopup.html')
